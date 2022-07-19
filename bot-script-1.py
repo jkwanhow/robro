496ecaa04c25f438707111ed5aedb4b847315a3b
Jeffery Kwan How <jeffery0kwan0how@gmail.com>
2022-07-19 21:45:34 +1000

    Update bot-script-1.py

--- bot-script-1.py
+++ bot-script-1.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import time
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -20,10 +21,16 @@ async def on_ready():
     
 @client.event
 async def on_message(message):
+    slurs = set(["fuck", "shit", "cunt", "bitch", "bastard"])
     if message.author == client.user:
         return
+        
+    if slurs.intersection(set(message.content.split())):
+        await message.channel.send('No swearing here please...')
+        await message.channel.send('`bitch`')
 
-    if message.content.startswith('$hello'):
+
+    if message.content.startswith('hello chungi'):
         await message.channel.send('Hello!')
 
 client.run(TOKEN)
