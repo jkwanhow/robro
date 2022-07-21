debc5cea36c3f6072c8cf4455e971e9260bfcd9b
elbertwang00@gmail.com <elbertwang00@users.noreply.github.com>
2022-07-22 02:23:43 +0800

    can collect song lyrics based on artist reliably

--- nigel.py
+++ nigel.py
@@ -1,7 +1,68 @@
+from lyricsgenius import Genius
+from rauth import OAuth1Service
+import os
+import re
+
+# token generated on https://genius.com/api-clients
+genius = Genius('mKm4v8Dj7SPcAIPOX2iB8HLXCdQY1QT579bMMr3tJxbiGqBuoTlevsR2sixGpvQG')
+
+lyrics_folder = 'nigel_artist_cache'
 
 
-class Nigel():
-    def lineis(self, line):
-        self.line1 = line
+class Nigel:
+    global lyrics_folder, genius
 
-        return f"Current line to nigel with is '{line}'"
+
+    def __init__(self, line1, artist_name=None, sort_criterion='popularity'):
+        self.line1 = line1
+        self.artist_name = artist_name
+        self.sort_criterion = sort_criterion
+
+
+    def find_lyrics(self):
+        if self.artist_name not in os.listdir(lyrics_folder):
+            os.mkdir(f"{lyrics_folder}/{self.artist_name}")
+
+        songs_exist = [title[:-4] for title in os.listdir(f"{lyrics_folder}/{self.artist_name}")]
+
+        while True:
+            try:
+                artist = genius.search_artist(self.artist_name, max_songs=1)
+                break
+            except Exception:
+                print('connection timeout, trying again...')
+
+        page = 1
+
+        while page:
+            request = genius.artist_songs(artist.id, sort=self.sort_criterion, per_page=50, page=page)
+            songs = request['songs']
+            print(len(songs))
+
+            for song in songs:
+                if song['title'] not in songs_exist:
+                    print(song['title'])
+
+                    while True:
+                        try:
+                            lyrics = genius.lyrics(song_url=song['url'])
+                            break
+                        except Exception:
+                            print('could not find lyrics, trying again...')
+
+                    file = f"{lyrics_folder}/{self.artist_name}/{re.sub('[/:*?<>|]','',song['title'])}.txt"
+
+                    with open(file, 'w', encoding='utf-8') as f:
+                        f.write(lyrics)
+            
+            page = request['next_page']
+    
+
+    def find_rhymes(self):
+        if self.artist_name != None:
+            song_bank = os.listdir(f"{lyrics_folder}/{self.artist_name}")
+        
+
+nigel = Nigel(line1='so clueless lil bitch boy gon need a map', artist_name='Drake')
+
+nigel.find_lyrics()
--- nigel_test.py
+++ nigel_test.py
@@ -1,22 +1,39 @@
+from encodings import utf_8
 from lyricsgenius import Genius
 from rauth import OAuth1Service
 import os
 
 lyrics_folder = 'nigel_artist_cache'
-artist_name = "Sia"
+artist_name = 'Kendrick Lamar'
+sort_criterion = 'popularity'
+no_songs = 5
 
 # token generated on https://genius.com/api-clients
 genius = Genius('mKm4v8Dj7SPcAIPOX2iB8HLXCdQY1QT579bMMr3tJxbiGqBuoTlevsR2sixGpvQG')
 
-if artist_name not in os.listdir(lyrics_folder):
-    os.mkdir(f"{lyrics_folder}/{artist_name}")
+def find_lyrics():
+    if artist_name not in os.listdir(lyrics_folder):
+        os.mkdir(f"{lyrics_folder}/{artist_name}")
+        search = 'Y'
 
-    artist = genius.search_artist(artist_name, max_songs=1, sort='popularity')
+    else:
+        songs_exist = os.listdir(f"{lyrics_folder}/{artist_name}")
 
+        for title in songs_exist:
+            print(title[:-4])
+        
+        search = str(input('Continue to search for songs from artist?\nY / N : '))
 
-    for song in artist.songs:
-        file = f"{lyrics_folder}/{artist_name}/{song.title}.txt"
+    if search == 'Y':
+        artist = genius.search_artist(artist_name, max_songs=no_songs, sort=sort_criterion)
 
-        with open(file, 'w') as f:
-            f.write(song.lyrics)
+        for song in artist.songs:
+            if song.title not in songs_exist:
+                file = f"{lyrics_folder}/{artist_name}/{song.title}.txt"
 
+                print(song.lyrics)
+
+                with open(file, 'w', encoding='utf-8') as f:
+                    f.write(song.lyrics)
+    
+find_lyrics()
